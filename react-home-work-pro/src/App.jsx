f9fe6055044dd92f878103ae3d87b769e2e93545
VladyBarvy <vladbarskoff@yandex.ru>
2024-10-18 13:10:54 +0300

    181024_2

--- react-home-work-pro/src/App.jsx
+++ react-home-work-pro/src/App.jsx
@@ -4,10 +4,43 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0);
+
+
+  
 
   return (
     <>
+
+
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,8 +61,4 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
-}
-
-export default App
+*/
