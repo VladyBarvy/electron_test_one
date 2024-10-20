1e9dacd308b0dab4c88493e0ef26a623d43b673b
VladyBarvy <vladbarskoff@yandex.ru>
2024-10-20 21:40:03 +0300

    201024 done hm

--- react-home-work-pro/src/CardOne.jsx
+++ react-home-work-pro/src/CardOne.jsx
@@ -57,3 +57,155 @@ const CardOne = () => {
 };
 
 export default CardOne;
+
+
+
+  // <div className="product-description-container">
+    //   <p className={`description ${isExpanded ? 'expanded' : 'collapsed'}`}>
+    //     {isExpanded ? description : `${description.slice(0, 20)}...`}
+    //   </p>
+    //   <button onClick={toggleDescription}>
+    //     {isExpanded ? 'Свернуть' : 'Развернуть'}
+    //   </button>
+    // </div>
+
+
+
+
+
+
+    // <div className="product-description-container">
+    //   <div className={`description ${isExpanded ? 'expanded' : 'collapsed'}`}>
+    //     {description}
+    //   </div>
+    //   <button onClick={toggleDescription}>
+    //     {isExpanded ? 'Свернуть' : 'Развернуть'}
+    //   </button>
+
+    //   <div class="container">
+    //     <button
+    //       class="button"
+    //       aria-expanded="false"
+    //       aria-controls="list"
+    //     >
+    //       Меню
+    //     </button>
+
+    //     <ul class="menu" id="list" aria-hidden="true">
+    //       <li class="menu-item">
+    //         <a href="#" class="menu-link" tabindex="-1">Винни-Пух</a>
+    //       </li>
+    //       <li class="menu-item">
+    //         <a href="#" class="menu-link" tabindex="-1">Тигра</a>
+    //       </li>
+    //       <li class="menu-item">
+    //         <a href="#" class="menu-link" tabindex="-1">Пятачок</a>
+    //       </li>
+    //     </ul>
+    //   </div>
+
+    // </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // <div className="container">
+    //   <button
+    //     className={`button ${isOpen ? 'active' : ''}`}
+    //     aria-expanded={isOpen}
+    //     aria-controls="list"
+    //     onClick={toggleMenu}
+    //   >
+    //     Меню
+    //   </button>
+
+    //   <ul className="menu" aria-hidden={!isOpen}>
+    //     <li className="menu-item">
+    //       <a href="#" className="menu-link" tabIndex={isOpen ? 0 : -1}>
+    //         {product}
+    //       </a>
+    //     </li>
+    //   </ul>
+    // </div>
+
+
+
+
+
+
+
+
+
+// https://doka.guide/recipes/dropdown-animation/?ysclid=m2eys6udi3146421769
+
+
+/*
+    <div className="container">
+      <button
+        className={`button ${isOpen ? 'active' : ''}`}
+        aria-expanded={isOpen}
+        aria-controls="list"
+        onClick={toggleMenu}
+      >
+        Меню
+      </button>
+
+      <ul className="menu" aria-hidden={!isOpen}>
+        <li className="menu-item">
+          <a href="#" className="menu-link" tabIndex={isOpen ? 0 : -1}>Винни-Пух</a>
+        </li>
+        <li className="menu-item">
+          <a href="#" className="menu-link" tabIndex={isOpen ? 0 : -1}>Тигра</a>
+        </li>
+        <li className="menu-item">
+          <a href="#" className="menu-link" tabIndex={isOpen ? 0 : -1}>Пятачок</a>
+        </li>
+      </ul>
+    </div>
+    */
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+/*
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+*/
---
+++ react-home-work-pro/src/CardTwo.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import './CardTwo.css';
+
+const CardTwo = () => {
+
+  const description = "\
+  Материал корпуса – нержавеющая сталь.\
+  Кожаный ремешок.\
+  Стекло – Gorila Glass.\
+  Батарея – Li-Pol 320 mAh, зарядка через USB.\
+  Лампы индикаторные газоразрядные ИВ-16."
+    ;
+
+  const faceImage = '../images/a1.jpg';
+
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  const toggleDescription = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const toggleFavorite = () => {
+    setIsFavorited(!isFavorited);
+  };
+
+  const displayedDescription = isExpanded ? description : `${description.substring(0, 20)}...`;
+
+
+
+  return (
+
+    <div className="product-card">
+
+      <div>
+        <h2 className="headfont">NEONICA - Metro 2033</h2>
+      </div>
+
+
+      <div>
+        <img className="face-image" src={faceImage} />
+      </div>
+
+      <h3 className="product-title">Наручные часы в стиле игры <br /> "Metro - 2033"</h3>
+      <p className={`product-description ${isExpanded ? 'expanded' : 'collapsed'}`}>
+        {displayedDescription}
+      </p>
+      <button className="toggle-button" onClick={toggleDescription}>
+        {isExpanded ? 'Скрыть' : 'Подробнее'}
+      </button>
+
+      <div >
+        <button onClick={toggleFavorite}>
+          {isFavorited ? '❤️ Убрать из избранного' : '🤍 Добавить в избранное'}
+        </button>
+      </div>
+    </div>
+  );
+
+};
+
+export default CardTwo;
